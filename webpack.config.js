096ba4aff20f3a19276d1f67b08a8da338499795
Brian Oblad <a.gray.cloud@gmail.com>
2019-09-09 14:15:46 -0400

    react boiler working

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const clientFolder = path.join(__dirname, './client');
+const publicFolder = path.join(__dirname, './public');
+
+module.exports = {
+  entry: path.join(clientFolder, 'index.js'),
+  mode: 'development',
+  output: {
+    path: publicFolder,
+    filename: 'bundle.js',
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
